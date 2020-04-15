bb4bf7d10f8df961f057e73763d629b381f6fefb
Natalie Mall <nkmall@ucdavis.edu>
2020-04-15 16:57:36 -0700

    First commit - added file with the browser script and draft readme

---
+++ image_saver_bookmark.js
@@ -0,0 +1,16 @@
+// This script is useful for downloading images on instagram! 
+
+// Code is here: 
+
+javascript:function removeElementsByClass(className){
+    var elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+};
+removeElementsByClass('_9AhH0');  // this 
+
+
+// Here it is written as one line (to put as a google bookmark)
+javascript:function removeElementsByClass(className){var elements = document.getElementsByClassName(className); while(elements.length > 0){ elements[0].parentNode.removeChild(elements[0]); } }; removeElementsByClass('_9AhH0');
+//
